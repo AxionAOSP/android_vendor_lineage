326ed5093195f13dfb2b133fe1a39675d2aa0877
minaripenguin <minaripenguin@users.noreply.github.com>
2025-02-05 08:39:40 +0800

    Disable memtag for different processes
    * it defaults to async if not specified
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- config/common.mk
+++ config/common.mk
@@ -324,6 +324,13 @@ PRODUCT_DISABLE_SCUDO := true
 SYSTEM_OPTIMIZE_JAVA := true
 SYSTEMUI_OPTIMIZE_JAVA := true
 
+# Disable async MTE on a few processes
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    persist.arm64.memtag.app.com.android.se=off \
+    persist.arm64.memtag.app.com.google.android.bluetooth=off \
+    persist.arm64.memtag.app.com.android.nfc=off \
+    persist.arm64.memtag.process.system_server=off
+    
 include vendor/lineage/config/version.mk
 
 -include vendor/lineage-priv/keys/keys.mk
