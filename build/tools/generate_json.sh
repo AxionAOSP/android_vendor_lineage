37834b3ce7e62b59029699eec078ff56e506d6c2
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-02-22 14:42:37 +0800

    Generate OTA json during build time
    Change-Id: Ia335c5a7b090afa53fc0b05f9f7a235d8fd92bd7
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

---
+++ build/tools/generate_json.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <TARGET_DEVICE> <PRODUCT_OUT> <FILENAME>"
+    exit 1
+fi
+
+TARGET_DEVICE=$1
+PRODUCT_OUT=$2
+LINEAGE_ZIP=$3
+FILENAME="axion-$LINEAGE_ZIP"
+
+if [[ "$FILENAME" =~ axion-.*-(COMMUNITY|OFFICIAL)-.*\.zip ]]; then
+    ROMTYPE="${BASH_REMATCH[1]}"
+else
+    echo "Error: Unable to extract ROM type from filename: $FILENAME"
+    exit 1
+fi
+
+if [[ "$FILENAME" =~ axion-([0-9\.]+)-[0-9]+-$ROMTYPE-.*\.zip ]]; then
+    VERSION="${BASH_REMATCH[1]}"
+else
+    echo "Error: Unable to extract version from filename: $FILENAME"
+    exit 1
+fi
+
+FILE_PATH="$PRODUCT_OUT/$FILENAME"
+
+if [ ! -f "$FILE_PATH" ]; then
+    echo "Error: File $FILE_PATH not found."
+    exit 1
+fi
+
+SIZE=$(stat -c%s "$FILE_PATH")
+ID=$(md5sum "$FILE_PATH" | awk '{print $1}')
+DATETIME=$(date +%s)
+JSON_FILE="$PRODUCT_OUT/${TARGET_DEVICE}.json"
+
+cat > "$JSON_FILE" <<EOF
+{
+    "response": [
+        {
+            "datetime": $DATETIME,
+            "filename": "$FILENAME",
+            "id": "$ID",
+            "romtype": "$ROMTYPE",
+            "size": $SIZE,
+            "url": "",
+            "version": "$VERSION"
+        }
+    ]
+}
+EOF
+
+echo "JSON saved to: $JSON_FILE"
+
+cat "$JSON_FILE"
