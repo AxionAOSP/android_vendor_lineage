risingOSS/vendor_lineage

Add preinstalled package list to stop auto cloning
Only extra adds needed, gms now ships it's own lists for this.

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

config/common.mk
@@ -257,6 +257,10 @@ CUSTOM_LOCALES += \
 PRODUCT_EXTRA_RECOVERY_KEYS += \
     vendor/lineage/build/target/product/security/lineage
 
+# Cloned app exemption
+PRODUCT_COPY_FILES += \
+    vendor/lineage/prebuilt/common/etc/sysconfig/preinstalled-packages-platform-android-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-platform-android-product.xml
+
 include vendor/lineage/config/version.mk
 
 -include vendor/lineage-priv/keys/keys.mk

AxionAOSP/android_vendor_lineage

Change-Id: I46263684a80372120936f91d23f8b7b68a1c511d

config/common.mk
@@ -364,6 +364,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/android.hardware.biometrics.face.xml
 endif
 
+# Cloned app exemption
+PRODUCT_COPY_FILES += \
+    vendor/lineage/prebuilt/common/etc/sysconfig/preinstalled-packages-platform-custom-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-platform-custom-product.xml
+
 ifeq ($(WITH_GMS),true)
 -include vendor/gapps/arm64/arm64-vendor.mk
 endif