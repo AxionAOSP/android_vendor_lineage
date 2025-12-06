3920f4504db863b38755b31e74ec9df8a6f0e0cc
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-12-06 14:17:25 +0800

    removing system server compiler filter flag
    Change-Id: Icd49cb7a695936d960bb41eb06d69f9320fdfec4

--- config/defaults_common.mk
+++ config/defaults_common.mk
@@ -1,6 +1,3 @@
-# Speed profile services and wifi-service to reduce RAM and storage.
-PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
-
 # Do not generate libartd.
 PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
 
