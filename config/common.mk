878b41bbd4abe200049813324d0ccaab88d8b16a
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-12-13 07:25:17 +0800

    [vendors] adding add-ons vendor deps
    Change-Id: Ie9fdb33d908a3bd2da1712468096746503c1297d
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- config/common.mk
+++ config/common.mk
@@ -2,6 +2,7 @@
 # Allow vendor/extra to override any property by setting it first
 $(call inherit-product, vendor/lineage/config/defaults_common.mk)
 $(call inherit-product-if-exists, vendor/extra/product.mk)
+$(call inherit-product-if-exists, vendor/addons/config.mk)
 
 # Allow vendor prebuilt repos to exclude themselves from bp scanning
 -include $(sort $(wildcard vendor/*/*/exclude-bp.mk))
